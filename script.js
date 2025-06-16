3db51a2f798cfae7e3ba2ad12dd479a942fed5bb
cham149 <ngoctramkt2004@gmail.com>
2025-06-16 14:46:57 +0700

    cong so

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function checkEvenOdd() {
   
     if (isNaN(num)) {
       resultElement.innerText = "Vui lòng nhập một số hợp lệ.";
-    } else if (num % 2 === 0) {
-      resultElement.innerText = `${num} là số chẵn.`;
+    } else if (num % 2 == 0) {
+      resultElement.innerText = `${num+2}`;
     } else {
-      resultElement.innerText = `${num} là số lẻ.`;
+      resultElement.innerText = `${num+1}`;
     }
   }
   
